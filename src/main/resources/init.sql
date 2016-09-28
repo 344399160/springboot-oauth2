e78d015886232af9e145f86c174cab45acaa5020
qiaobin <344399160@qq.com>
2016-09-28 15:36:06 +0800

    插入用户

---
+++ src/main/resources/init.sql
@@ -0,0 +1,4 @@
+insert into groups(id, name) values('0', 'admin');
+insert into users(id, group_id, username, password) values('0', '0', 'admin', 'YWRtaW4=');
+insert into user_authorities(user_id, user_authority) values ('0', 'ADMIN');
+
